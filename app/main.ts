34b143eb3a6eb4285712fdc35db0bca5e4a09714
qww <497363953@qq.com>
2025-04-04 21:19:48 +0800

    chore: fix break changes from unhead

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,7 @@
 import { i18n } from "./i18n/index"
 import { Error } from "./utils"
 import { createApp } from "vue"
-import { createHead } from "@unhead/vue"
+import { createHead } from "@unhead/vue/client"
 import App from "./App.vue"
 import "element-plus/dist/index.css"
 import "element-plus/theme-chalk/dark/css-vars.css"
